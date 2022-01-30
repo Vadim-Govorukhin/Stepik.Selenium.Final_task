94683caa7af14f22c4d24f3a02dff15696ddfba1
Vadim Govoruhin <v081297@mail.ru>
2022-01-30 23:32:00 +0300

    first Page Object step

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jan 30 23:21:47 2022
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
 
-@author: Govor_000
-"""
-
+class MainPage(BasePage): 
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
